f63211120472c7afbba05b068a2703d0252d5a74
Govind Thakur <govindthakur9540@gmail.com>
2021-05-24 22:39:39 +0530

    Completed

--- src/Ingredients/data.js
+++ src/Ingredients/data.js
@@ -3,43 +3,43 @@ export default {
     items: [
         {
             id: 1,
-            image: "http://www.pngmart.com/files/5/Red-Apple-PNG-Photos.png",
+            image: "https://pngimg.com/uploads/apple/apple_PNG12489.png",
             name: "Apple",
             cal: 81
         },
         {
             id: 2,
-            image: "http://www.pngmart.com/files/1/Banana-Icon-PNG.png",
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/69/Banana.png",
             name: "Banana",
             cal: 105
         },
         {
             id: 3,
-            image: "http://www.pngmart.com/files/3/Chocolate-Bar-PNG-Transparent-Image.png",
+            image: "https://pngimg.com/uploads/chocolate/chocolate_PNG97200.png",
             name: "Chocolate",
             cal: 265
         },
         {
             id: 4,
-            image: "http://www.pngmart.com/files/1/Purple-Grapes-PNG.png",
+            image: "https://pngimg.com/uploads/grape/grape_PNG521.png",
             name: "Grapes",
             cal: 114
         },
         {
             id: 5,
-            image: "http://www.pngmart.com/files/5/Noodles-PNG-Image.png",
+            image: "https://pngimg.com/uploads/noodle/noodle_PNG15.png",
             name: "Noodels",
             cal: 159
         },
         {
             id: 6,
-            image: "http://www.pngmart.com/files/5/Hamburger-Transparent-Background.png",
+            image: "https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4135.png",
             name: "Burger",
             cal: 250
         },
         {
             id: 7,
-            image: "http://www.pngmart.com/files/8/Pepsi-PNG-Image.png",
+            image: "https://pngimg.com/uploads/pepsi/pepsi_PNG5.png",
             name: "Pepsi",
             cal: 170
         }
